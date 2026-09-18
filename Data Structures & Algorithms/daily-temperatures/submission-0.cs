c117ef16bc71cbebcbcf547f4bdd6bc3ec0b58f0
Osama Aly <70722589+osama-aly-oa@users.noreply.github.com>
2026-09-18 15:49:21 +0300

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] DailyTemperatures(int[] temperatures) {
+        int[] res = new int[temperatures.Length];
+        Stack<int[]> stack = new Stack<int[]>();
+
+        for (int i = 0; i < temperatures.Length; i++) {
+            int t = temperatures[i];
+            while (stack.Count > 0 && t > stack.Peek()[0]) {
+                int[] pair = stack.Pop();
+                res[pair[1]] = i - pair[1];
+            }
+            stack.Push(new int[] { t, i });
+        }
+        return res;
+
+    }
+}
