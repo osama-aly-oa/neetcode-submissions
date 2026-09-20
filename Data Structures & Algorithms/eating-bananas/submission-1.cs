37486783214214f2c62cc4eb8f18f961b12ba01e
Osama Aly <70722589+osama-aly-oa@users.noreply.github.com>
2026-09-20 15:46:39 +0300

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int MinEatingSpeed(int[] piles, int h) {
+        int l = 1, r = piles.Max();
+
+        while(l < r) {
+            int k = l + (r -l) / 2;
+
+            long totalTime = 0;
+            foreach(int pile in piles) {
+            totalTime += (pile + k - 1) / k;         
+            }
+
+            if(totalTime <= h){
+                r = k;
+            } else {
+                l = k + 1;
+            }
+        }
+        return l;
+    }
+}
