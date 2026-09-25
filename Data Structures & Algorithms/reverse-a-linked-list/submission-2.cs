882e14eca17e3c0f8bf852a409a564efeb7f0367
Osama Aly <70722589+osama-aly-oa@users.noreply.github.com>
2026-09-25 17:14:36 +0300

    Add: reverse-a-linked-list - submission-2

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-2.cs
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+ 
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        if(head == null) return null;
+
+        if(head.next == null  ) return head;
+
+        ListNode returnList = ReverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return returnList;
+    }
+}
